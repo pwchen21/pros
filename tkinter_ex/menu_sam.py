c27273f5d2e00d832749878e46dfe256c4fe1a78
pwchen21 <pwchen21@gmail.com>
2018-07-24 17:29:04 +0800

    Adding reference document

--- tkinter_ex/menu_sam.py
+++ tkinter_ex/menu_sam.py
@@ -8,30 +8,34 @@ l=tk.Label(window,text='',bg='yellow')
 l.pack()
 
 counter=0
-def do_job():
+def do_job(x):
     global counter
-    l.config(text='do '+str(counter))
+    l.config(text='do '+x)
     counter+=1
     
 menubar=tk.Menu(window)
 filemenu=tk.Menu(menubar,tearoff=0)
 menubar.add_cascade(label='File',menu=filemenu)
-filemenu.add_command(label='New',command=do_job)
-filemenu.add_command(label='Open',command=do_job)
-filemenu.add_command(label='Save',command=do_job)
+filemenu.add_command(label='New',command=do_job("N"))
+filemenu.add_command(label='Open',command=do_job("O"))
+filemenu.add_command(label='Save',command=do_job("S"))
 filemenu.add_separator()
 filemenu.add_command(label='Exit',command=window.quit)
+
+help(filemenu)
 """
 editmenu=tk.Menu(menubar,tearoff=0)
 menubar.add_cascade(label='Edit',menu=editmenu)
 editmenu.add_command(label='Cut',command=do_job)
 editmenu.add_command(label='Copy',command=do_job)
 editmenu.add_command(label='Paste',command=do_job)
+"""
+
 
 submenu=tk.Menu(filemenu)
 filemenu.add_cascade(label='Import',menu=submenu,underline=0)
 submenu.add_command(label='Submenu1',command=do_job)
 
 window.config(menu=menubar)
-"""
+
 window.mainloop() 
