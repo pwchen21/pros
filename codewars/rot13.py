72b8ee3cf3439ef393798a49393f6d18f0203c09
pwchen21 <pwchen21@gmail.com>
2019-02-21 11:18:36 +0800

    updated

---
+++ codewars/rot13.py
@@ -0,0 +1,13 @@
+import string
+from codecs import encode as _dont_use_this_
+
+
+def rot13(message):
+    res=''
+    for x in message:
+        if 65 <= ord(x) <= 90:
+            if ord(x) + 13 > 90:
+                res =+ 
+    pass
+    
+    If user select Yes, BIOS remove the SIID key so that the message will not appear again.
--- codewars/ww.py
+++ codewars/ww.py
@@ -1,21 +1,55 @@
 def hwt(wts):
     wtsl=wts.split(" ")
+    tmp=[]
     res=[]
-    dic={}
-    print("P0:", wts)
+    res1=''
+    dic={}   
+    #print("P0:", wtsl)
     ind=0
-    for x in wts:
-        print("P1:", x, wwt(x))
-        dic[ind][0][0] = x
-        dic[ind][0][1] = wwt(x)
+    for x in wtsl:
+        #print("P1:", x)
+        dic[ind] = [x, wwt(x)]
         ind+=1
+        #print("P2:", wwt(x))
     print(dic)
+    
+    #Sort by weight value
+    for x in dic:
+        tmp.append(dic[x][1])
+        #print('tmp:', tmp)
+    
+    tmp1=sorted(set(tmp), reverse=False)
+    print(tmp)
+    print(tmp1)
+    
+    for x in tmp1:
+        print('x:', x)
+        if tmp.count(x) > 1:
+            dup=[]
+            for y in dic:
+                if dic[y][1]==x:
+                    dup.append(dic[y][0])
+            print('dup', dup)
+            
+            for z in sorted(dup):
+                res.append(z)
+            
+        else:
+            for y in dic:
+                if dic[y][1]==x:
+                    res.append(dic[y][0])
+    
+    
         
+    print(' '.join(res))
+    
+ 
 def wwt(vlu):
     wwvlu=0
+    #print('P3:', len(vlu))
     for x in range(len(vlu)):
         wwvlu+=int(vlu[x])    
-    print(wwvlu)
+    #print(wwvlu)
     return(wwvlu)
     
-hwt("70 50 90 80 30")
+hwt("2000 10003 1234000 44444444 9999 11 11 22 123")
