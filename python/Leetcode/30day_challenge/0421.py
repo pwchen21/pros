a7e097ee5dd8729fbee137c1a553b6f8c3af08d0
pinkk51 <pwchen21@gmail.com>
2020-04-25 04:32:03 +0800

    Commit Leetcode 30day_challenge

--- python/Leetcode/30day_challenge/0421.py
+++ python/Leetcode/30day_challenge/0421.py
@@ -1,14 +1,8 @@
 class Solution:
-    def bstFromPreorder(self, preorder):
-        if not preorder:
-            return None
-        root = TreeNode(preorder[0])
-        i = 1
-        while i<len(preorder) and  preorder[i] < root.val:
-            i+=1
-        root.left = self.bstFromPreorder(preorder[1:i])
-        root.right = self.bstFromPreorder(preorder[i:])
-        return root
-        
-A=Solution()
-A.bstFromPreorder([8,5,1,7,10,12])
+    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
+        n,m = binaryMatrix.dimensions();i = n-1;j= m-1
+        while (i>=0 and j>=0):
+            if binaryMatrix.get(i,j)==0 :i-=1
+            else:j-=1
+            
+        return -1 if j == m-1  else j+1
