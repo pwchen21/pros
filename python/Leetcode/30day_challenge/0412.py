b31361207a877341ac93489c3dacaa25bff5105e
pinkk51 <pwchen21@gmail.com>
2020-05-01 04:31:16 +0800

    Change folder Name

--- python/Leetcode/30day_challenge/0412.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def lastStoneWeight(self, stones: List[int]) -> int:
-        ss=sorted(stones)
-        while len(ss) > 1:
-            ss.append(abs(ss.pop() - ss.pop()))
-            ss=sorted(ss)
-        return ss[0]
--- python/Leetcode/30day_challenge/0421.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def leftMostColumnWithOne(self, binaryMatrix: 'BinaryMatrix') -> int:
-        n,m = binaryMatrix.dimensions();i = n-1;j= m-1
-        while (i>=0 and j>=0):
-            if binaryMatrix.get(i,j)==0 :i-=1
-            else:j-=1
-            
-        return -1 if j == m-1  else j+1
